ed0ca4b0107705461b86069b23c5114b939d6b58
Peter Lavelle <peter@solderintheveins.co.uk>
2013-07-28 13:46:13 +0100

    Adding basic checks for provisioned hosts

--- icinga/manifests/observed.pp
+++ icinga/manifests/observed.pp
@@ -15,13 +15,6 @@ class icinga::observed {
        require => Class['icinga::packages']
   }
 
-  @@nagios_service { "check_ping_${hostname}":
-       check_command => "check_ping!100.0,20%!500.0,60%",
-       use => "generic-service",
-       host_name => "$fqdn",
-       service_description => "$prefix PING: ${hostname}",
-       require => Class['icinga::packages']
-  }
 
   @@nagios_service { "check_ssh_${hostname}":
        check_command => "check_ssh!$fqdn",
@@ -30,46 +23,6 @@ class icinga::observed {
        service_description => "$prefix SSHD: ${hostname}",
        require => Class['icinga::packages']
   }
-   @@nagios_service { "check_ssh_load_${hostname}":
-       check_command => "check_ssh_load!22!5.0,4.0,3.0!8.0,7.0,6.0",
-       use => "generic-service",
-       host_name => "$fqdn",
-       service_description => "$prefix LOAD: ${hostname}",
-       require => Class['icinga::packages']
-  }
-
-   @@nagios_service { "check_ssh_disk_${hostname}":
-       check_command => "check_ssh_disk!22!10!8!/",
-       use => "generic-service",
-       service_description => "$prefix ROOT PARTITION SPACE: ${hostname}",
-       host_name => "$fqdn",
-       require => Class['icinga::packages']
-  }
-
-
-  @@nagios_service { "check_ssh_disk_tmp_${hostname}":
-       check_command => "check_ssh_disk!22!10!8!/tmp",
-       use => "generic-service",
-       service_description => "$prefix /tmp PARTITION SPACE: ${hostname}",
-       host_name => "$fqdn",
-       require => Class['icinga::packages']
-  }
-
-  @@nagios_service { "check_ssh_swap_${hostname}":
-       check_command => "check_ssh_swap!22!30%!20%",
-       use => "generic-service",
-       service_description => "$prefix SWAP USAGE: ${hostname}",
-       host_name => "$fqdn",
-       require => Class['icinga::packages']
-  }
-
-   @@nagios_service { "check_ssh_apt_${hostname}":
-       check_command => "check_ssh_apt",
-       use => "generic-service",
-       service_description => "$prefix APT Package Updates: ${hostname}",
-       host_name => "$fqdn",
-       require => Class['icinga::packages']
-  }
-
+   
 
 }
